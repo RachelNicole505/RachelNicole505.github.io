df03a5a7ecce473b23a4291dae3997bc85217204
RachelNicole505 <rf4057@bard.edu>
2016-08-15 10:28:55 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -29,7 +29,7 @@
 
 var rules = {
   "~*x hello ~*y": [
-    "How do you do. Please state your problem."
+    "I dont want to talk with lesser beings."
   ],
   "~*x computer ~*y": [
     "Do computers worry you?",
