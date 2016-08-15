28b6386f30a3b4f83a2336e57e5975b9afe5279e
RachelNicole505 <rf4057@bard.edu>
2016-08-15 10:25:22 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -5,7 +5,7 @@
  */
 
 var things = ["Music", "Art", "Dance", "Theater", "Math", "Architecture"];
-var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
+var programmers = ["beyonce", "Meryl Streep", "choreographer", "director", "mathematician", "architect"]
 var castles = ["museums", "bridges", "cities"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
 var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
